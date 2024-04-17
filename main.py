daef22068925b3d4f14e1ec0796fb5b3da0f70fd
Morning Dove Nelson <morningdhansen@gmail.com>
2024-04-17 09:34:17 -0600

    get, post, delete are properly working

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-import json
+from fastapi import FastAPI
 
+import json
+from models import Patient
+
+app = FastAPI()
 
 with open("patients.json", "r") as f:
     patient_list = json.load(f)
 
-# Use the first name as the unique identifier. For example, in the PUT route, you'd have something like this: "/patients/{first_name}"
+patients: list[Patient] = []
+
+for patient in patient_list:
+    patients.append(Patient(**patient))
+
+
+@app.get("/patients/")
+async def get_patient() -> list[Patient]:
+    return patients
+
+@app.post("/patients/")
+async def create_patient(patient: Patient) -> None:
+    patients.append(patient)
+
+@app.put("/patients/{fname}")
+async def update_patient(first_name: str, updated_patient: Patient) -> Patient:
+    for i, patient in enumerate(patients):
+        if first_name ==  patient.first_name:
+            patient[i] = updated_patient
+            return updated_patient
+        raise KeyError("Patient not found")
+
+@app.delete("/patients/{first_name}")
+async def delete_patient(first_name: str) -> str:
+    for i, patient in enumerate(patients):
+        if first_name == patient.first_name:
+            patients.pop(i)
+            return "Patient deleted"
+        raise KeyError("Patient not found")
+
